0164e3faafe542251a17e2ae32e57f2416b27d28
Laxman Mandal <laxmanmandal1234@gmail.com>
2022-01-22 23:36:28 +0530

    add .gitignore, procfile and update port

--- app.js
+++ app.js
@@ -154,6 +154,10 @@ app.post('/delete', function(req, res) {
 
 });
 
-app.listen(3000, function(req, res) {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function(req, res) {
   console.log("Server started at port 3000");
 });
